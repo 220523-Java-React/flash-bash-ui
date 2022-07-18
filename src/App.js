0327d527005a9885172e312485c1005fe4309571
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-07-18 15:19:56 -0500

    add app background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import Error from "./components/Error";
 import Routes from './security/Routes';
+import { Paper } from "@mui/material";
+import DesktopImage from './images/desktop-background.jpg'
+import MobileImage from './images/mobile-background.jpg'
 
 const darkTheme = createTheme({
   palette: {
@@ -12,20 +15,43 @@ const darkTheme = createTheme({
 });
 
 function App() {
-
   const [appUser, updateAppUser] = useState();
   const [error, updateError] = useState(null);
+  const [windowWidth, updateWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, updateWindowHeight] = useState(window.innerHeight);
+  const imageUrl = windowWidth >= 650 ? DesktopImage : MobileImage;
+
+  useEffect(() => {
+      function handleWindowResize(){
+          updateWindowWidth(window.innerWidth)
+          updateWindowHeight(window.innerHeight)
+      }
+
+      window.addEventListener('resize', handleWindowResize);
+
+      return () => {
+          window.removeEventListener('resize', handleWindowResize)
+      }
+  }, [])
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <Navbar user={appUser} />
-      <Routes appUser={appUser} updateAppUser={updateAppUser} updateError={updateError} />
+        <Paper style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+            backgroundAttachment: "fixed",
+            height: windowHeight
+        }}>
+            <Navbar user={appUser} />
+            <Routes appUser={appUser} updateAppUser={updateAppUser} updateError={updateError} />
 
-      {error && 
-        <Error 
-            error={error} 
-            open={!!error} 
-            updateOpen={() => updateError(null)} />}
+            {error &&
+                <Error
+                    error={error}
+                    open={!!error}
+                    updateOpen={() => updateError(null)} />}
+        </Paper>
     </ThemeProvider>
   );
 }
