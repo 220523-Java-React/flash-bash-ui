43a0934704add12fa9c5d4b53debae4ca5e53743
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-07-20 22:15:35 -0500

    enhance flashcard page

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,25 +1,42 @@
-import { Paper, Typography, Stack } from "@mui/material";
+import {Paper, Typography, Stack, Grid} from "@mui/material";
 import { useState } from "react";
 
-export default function Flashcard({flashcard}){
+export default function Flashcard({flashcard, width}){
 
     const [isFlipped, updateIsFlipped] = useState(false);
 
     // spacing moves the question/answer
     // elevation is the background color of the Paper
-    return <Paper spacing={8} elevation={20} onClick={() => updateIsFlipped(!isFlipped)} component={Stack} sx={{ margin:3, maxWidth: 450, minHeight:215}}>
-            <Typography pl={1}>
-                {flashcard.topic}
-            </Typography>
-
-            {!isFlipped && 
-            <Typography align={"center"} >
-                {flashcard.question}
-            </Typography>}
-            {isFlipped &&
-            <Typography align={"center"} >
-            {flashcard.answer}
-            </Typography>}
-            
+    return <Paper spacing={8} elevation={20} onClick={() => updateIsFlipped(!isFlipped)} component={Stack} sx={{ margin:3, width: width, minHeight:215}}>
+                <Grid id="top-row" container={true} spacing={0}>
+                    <Grid item={true} sm={12}>
+                        <Typography pl={1}>
+                            {flashcard.topic}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            <Grid id="content" container={true} spacing={1}>
+                    {!isFlipped &&
+                        <Grid item={true} sm={12} alignItems={"center"}>
+                            <Typography align={"center"} >
+                                {flashcard.question}
+                            </Typography>
+                        </Grid>
+                    }
+                {isFlipped &&
+                    <Grid item={true} sm={12} alignItems={"center"}>
+                        <Typography align={"center"}>
+                            {flashcard.answer}
+                        </Typography>
+                    </Grid>
+                }
+            </Grid>
+            <Grid id="bottom-row" container={true} spacing={1}>
+                <Grid item={true} sm={12} alignItems={"center"}>
+                    <Typography>
+                        {flashcard.difficulty}
+                    </Typography>
+                </Grid>
+            </Grid>
     </Paper>
 }
--- src/pages/FlashcardsPage.js
+++ src/pages/FlashcardsPage.js
@@ -1,44 +1,95 @@
 import { useEffect, useState } from "react"
 import Flashcard from "../components/Flashcard"
 import API from "../util/api"
-
-// TODO: replace the static list, with a call to our API???
-let flashcardsList = [
-    {
-        question: "What does IoC stand for in Spring?",
-        answer: "Inversion of Control",
-        topic: "JAVA"
-    },
-    {
-        question: "What is DOM?",
-        answer: "Document Object Model",
-        topic: "JavaScript"
-    },
-    {
-        question: "What is the @Transactional annotation in Spring?",
-        answer: "Enables transaction management (Commit, Rollback, etc...) in a class or method.",
-        topic: "JAVA"
-    }
-]
+import {Grid, Pagination, Slider} from "@mui/material";
 
 export default function FlashcardsPage(){
 
     const [flashcards, updateFlashcards] = useState(null);
+    const [cardsPerRow, updateCardsPerRow] = useState(2)
+    const [pageCount, updatePageCount] = useState(0);
+    const [currentPage, updateCurrentPage] = useState(1);
+    const [difficulty, updateDifficulty] = useState("MASTER");
+    const [difficulties, updateDifficulties] = useState(null)
+    const [creator, updateCreator] = useState("");
 
-    useEffect(getAllFlashcards, [])
+    const [difficultyRange, updateDifficultyRange] = useState([0,100]);
+    const NUMBER_OF_ROWS = 2
+
+
+    useEffect(getAllDifficulties, [])
+    useEffect(getAllFlashcards, [currentPage, difficulty, creator, cardsPerRow])
+    useEffect(() => updateCardsPerRow(calculateCardsPerRow()), [window.innerWidth])
+
+    function getAllDifficulties(){
+        API.get('/flashcards/difficulties')
+            .then(handleDifficultyResponse)
+    }
+
+    function handleDifficultyResponse(response){
+        const totalDifficulties = response.data.length;
+
+        updateDifficulties(response.data.map((difficulty, index) => {
+            return {
+                value: index * (100 / (totalDifficulties-1)),
+                label: difficulty
+            }
+        }))
+    }
 
     function getAllFlashcards(){
         // request to the api
-        API.get("/flashcards")
-            .then(response => updateFlashcards(response.data))
-        // updateFlashcards with the list that is returned
+        API.get(`/flashcards?page=${currentPage - 1}&size=${cardsPerRow * NUMBER_OF_ROWS}&difficulty=${difficulty}&creator=${creator}`)
+            .then(handleResponse);
     }
 
-    return <>
-        {flashcards && flashcards.map((flashcard) => 
-            <Flashcard key={flashcard.id} flashcard={flashcard} />
-        )}
+    function handleResponse(response){
+        const data = response.data;
+        updatePageCount(calculatePageCount(data.totalElements))
+        updateFlashcards(data.content)
+    }
 
-        {!flashcards && <h3>Loading Flashcards...</h3>}
+    function calculateCardsPerRow(){
+        if(window.innerWidth < 1113) return 1
+        if(window.innerWidth < 1661) return 2
+        if(window.innerWidth < 2209) return 3
+        return 4
+    }
+
+    function calculatePageCount(flashcardCount){
+        return Math.ceil(flashcardCount / (cardsPerRow * NUMBER_OF_ROWS));
+    }
+
+    function handlePageChange(event, pageNumber){
+        updateCurrentPage(pageNumber)
+    }
+
+    function handleDifficultyChange(event, range){
+        updateDifficultyRange(range);
+        updateDifficulty(difficulties.find(difficulty => difficulty.value === range[1]).label)
+    }
+    return <>
+
+            {flashcards &&
+                <>
+                    <Grid container={true} spacing={0} justifyContent={"center"}>
+                        <Grid item={true} sm={3}>
+                            <Slider
+                                    value={difficultyRange}
+                                    onChange={handleDifficultyChange}
+                                    step={null}
+                                    valueLabelDisplay={"off"}
+                                    marks={difficulties}/>
+                        </Grid>
+                    </Grid>
+                    <Grid container={true} spacing={0} justifyContent={"center"} columns={cardsPerRow}>
+                        {flashcards.map((flashcard) => <Flashcard key={flashcard.id} flashcard={flashcard} width={500} />)}
+                    </Grid>
+                    <Grid container={true} spacing={0} justifyContent={"center"}>
+                        <Pagination count={pageCount} page={currentPage} onChange={handlePageChange} color={"primary"}/>
+                    </Grid>
+                </>
+            }
+            {!flashcards && <h3>Loading Flashcards...</h3>}
     </>
 }
